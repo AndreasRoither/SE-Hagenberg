e87f74c5a90158ff69241e9d97822526cfc83a24
Andreas Roither <andi.roither@gmail.com>
2016-11-10 18:43:08 +0100

    mergefields.pas
    [Fixed]
    if statement, was never true if number was too high
    a3 was not cleared when trying out another test
    
    [added]
    procedure clearArray(), clears an array, sets values 0

--- 1. Semester/ADE/Uebung05/mergefields.pas
+++ 1. Semester/ADE/Uebung05/mergefields.pas
@@ -8,6 +8,13 @@ begin
   WriteLn();
 end;
 
+procedure clearArray(var a : ARRAY OF INTEGER);
+var i : INTEGER;
+begin
+  for i := 0 to length(a)-1 do
+    a[i] := 0;
+end;
+
 PROCEDURE Merge(a1, a2: ARRAY OF INTEGER; VAR a3: ARRAY OF INTEGER; VAR n3: INTEGER); 
 var i,i2,i3,i4,count : INTEGER;
 var found : Boolean;
@@ -36,12 +43,12 @@ BEGIN
 
     IF found = False THEN
       FOR i3 := 0 TO length(a3)-1 DO
-        IF ((a2[i] >= a3[i3]) OR (i = 0)) AND ((a2[i] <= a3[i3+1]) OR (a3[i3+1] = 0)) THEN 
+        IF (a2[i] <= a3[i3]) OR ((a2[i] >= a3[i3]) AND (a3[i3] = 0))THEN 
         BEGIN
           FOR i4 := length(a3)-1 DOWNTO i3+1 DO
             a3[i4] := a3[i4-1];
 
-          a3[i3+1] := a2[i];  
+          a3[i3] := a2[i];  
           break;        
         END;
     found := false;
@@ -54,24 +61,26 @@ var a3 : ARRAY [1 .. (length(a1) + length(a2))] OF INTEGER;
 var n3 : INTEGER;
 
 BEGIN
-// a1[1] := 1;
-// a1[2] := 2;
-// a1[3] := 3;
-// a1[4] := 4;
-// a1[5] := 5;
-// a1[6] := 6;
+a1[1] := 1;
+a1[2] := 2;
+a1[3] := 3;
+a1[4] := 4;
+a1[5] := 5;
+a1[6] := 6;
 
-// a2[1] := 7;
-// a2[2] := 8;
-// a2[3] := 9;
-// a2[4] := 10;
+a2[1] := 7;
+a2[2] := 8;
+a2[3] := 9;
+a2[4] := 10;
 
-// Merge(a1,a2,a3,n3);
+Merge(a1,a2,a3,n3);
 
-// printArray(a1);
-// printArray(a2);
-// printArray(a3);
+printArray(a1);
+printArray(a2);
+printArray(a3);
+WriteLn();
 
+clearArray(a3);
 
 a1[1] := 2;
 a1[2] := 4;
