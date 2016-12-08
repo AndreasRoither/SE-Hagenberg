03702c237f653dac9af719861c182818ecaccb68
AndiRoither <andi.roither@hotmail.de>
2016-12-08 11:29:31 +0100

    Finished Sum

---
+++ 1. Semester/ADE/Uebung08/BigInts.pas
@@ -0,0 +1,425 @@
+(* BigInts:                                       F.Li, 1998-11-22
+   -------                                         HDO, 2000-11-13
+   Artihmetic for arbitrary size integers which are
+   represented as singly-linked lists.
+==================================================================*)
+
+PROGRAM BigInts;
+
+(*$IFDEF WINDOWS, for Borland Pascal only*)
+  USES
+    WinCrt;
+(*$ENDIF*)
+
+(*$DEFINE SIGNED*)   (*when defined: first digit is sign +1 or -1*)
+
+  CONST
+    base = 1000;     (*base of number system used in all*)
+                     (*  calculations, big digits: 0 .. base - 1*)
+
+  TYPE
+    NodePtr = ^Node;
+    Node = RECORD
+      next: NodePtr;
+      val: INTEGER;
+    END; (*RECORD*)
+    BigIntPtr = NodePtr;
+
+
+  FUNCTION NewNode(val: INTEGER): NodePtr;
+    VAR
+      n: NodePtr;
+  BEGIN
+    New(n);
+    (*IF n == NIL THEN ...*)
+    n^.next := NIL;
+    n^.val := val;
+    NewNode := n;
+  END; (*NewNode*)
+
+  FUNCTION Zero: BigIntPtr;
+  BEGIN
+    Zero := NewNode(0);
+  END; (*Zero*)
+
+  PROCEDURE Append(VAR bi: BigIntPtr; val: INTEGER);
+    VAR
+      n, last: NodePtr;
+  BEGIN
+    n := NewNode(val);
+    IF bi = NIL THEN
+      bi := n
+    ELSE BEGIN (*l <>NIL*)
+      last := bi;
+      WHILE last^.next <> NIL DO BEGIN
+        last := last^.next;
+      END; (*WHILE*)
+      last^.next := n;
+    END; (*ELSE*)
+  END; (*Append*)
+
+  PROCEDURE Prepend(VAR bi: BigIntPtr; val: INTEGER);
+    VAR
+      n: NodePtr;
+  BEGIN
+    n := NewNode(val);
+    n^.next := bi;
+    bi := n;
+  END; (*Prepend*)
+
+  FUNCTION Sign(bi: BigIntPtr): INTEGER;
+  BEGIN
+(*$IFDEF SIGNED*)
+      (*assert: bi <> NIL*)
+      Sign := bi^.val; (*results in +1 or -1*)
+(*$ELSE*)
+      WriteLn('Error in Sign: no sign node available');
+      Halt;
+(*$ENDIF*)
+  END; (*Sign*)
+
+  FUNCTION CopyOfBigInt(bi: BigIntPtr): BigIntPtr;
+    VAR
+      n: NodePtr;
+      cBi: BigIntPtr; (*cBi = copy of BigIntPtr*)
+  BEGIN
+    cBi := NIL;
+    n := bi;
+    WHILE n <> NIL DO BEGIN
+      Append(cBi, n^.val);
+      n := n^.next;
+    END; (*WHILE*)
+    CopyOfBigInt := cBi;
+  END; (*CopyOfBigInt*)
+
+  PROCEDURE InvertBigInt(VAR bi: BigIntPtr);
+    VAR
+      iBi, next: NodePtr; (*iBi = inverted BigIntPtr*)
+  BEGIN
+    IF bi <> NIL THEN BEGIN
+      iBi := bi;
+      bi := bi^.next;
+      iBi^.next := NIL;
+      WHILE bi <> NIL DO BEGIN
+        next := bi^.next;
+        bi^.next := iBi;
+        iBi := bi;
+        bi := next;
+      END; (*WHILE*)
+      bi := iBi;
+    END; (*IF*)
+  END; (*InvertBigInt*)
+
+  PROCEDURE DisposeBigInt(VAR bi: BigIntPtr);
+    VAR
+      next: NodePtr;
+  BEGIN
+    WHILE bi <> NIL DO BEGIN
+      next := bi^.next;
+      Dispose(bi);
+      bi := next;
+    END; (*WHILE*)
+  END; (*DisposeBigInt*)
+
+
+  (* ReadBigInt: reads BigIntPtr, version for base = 1000
+     Input syntax: BigIntPtr = { digit }.
+                   BigIntPtr = [+ | -] digit { digit }.
+     The empty string is treated as zero, and as the whole
+     input is read into one STRING, max. length is 255.
+  -------------------------------------------------------*)
+  PROCEDURE ReadBigInt(VAR bi: BigIntPtr);
+    VAR
+      s: STRING;           (*input string*)
+      iBeg, iEnd: INTEGER; (*begin and end of proper input *)
+      bigDig, decDig: INTEGER;
+      nrOfBigDigits, lenOfFirst: INTEGER;
+      sign, i, j: INTEGER;
+
+    PROCEDURE WriteWarning(warnPos: INTEGER);
+    BEGIN
+      WriteLn('Warning in ReadBigInt: ',
+              'character ', s[warnPos],
+              ' in column ', warnPos, ' is treated as zero');
+    END; (*WriteWarning*)
+
+  BEGIN (*ReadBigInt*)
+    IF base <> 1000 THEN BEGIN
+      WriteLn('Error in ReadBigInt: ',
+              'procedure currently works for base = 1000 only');
+      Halt;
+    END; (*IF*)
+    ReadLn(s);
+    iEnd := Length(s);
+    IF iEnd = 0 THEN
+      bi := Zero
+    ELSE BEGIN
+
+(*$IFDEF SIGNED*)
+      IF s[1] = '-' THEN BEGIN
+        sign := -1;
+        iBeg :=  2;
+      END (*THEN*)
+      ELSE IF s[1] = '+' THEN BEGIN
+        sign := 1;
+        iBeg := 2;
+      END (*THEN*)
+      ELSE BEGIN
+(*$ENDIF*)
+        sign := 1;
+        iBeg := 1;
+(*$IFDEF SIGNED*)
+      END; (*ELSE*)
+(*$ENDIF*)
+
+      WHILE (iBeg <= iEnd) AND
+            ((s[iBeg] < '1') OR (s[iBeg] > '9')) DO BEGIN
+        IF (s[iBeg] <> '0') AND (s[iBeg] <> ' ') THEN
+          WriteWarning(iBeg);
+        iBeg := iBeg + 1;
+      END; (*WHILE*)
+
+      (*get value from s[iBeg .. iEnd]*)
+      IF iBeg > iEnd THEN
+        bi := Zero
+      ELSE BEGIN
+        bi := NIL;
+        nrOfBigDigits := (iEnd - iBeg) DIV 3 + 1;
+        lenOfFirst    := (iEnd - iBeg) MOD 3 + 1;
+        FOR i := 1 TO nrOfBigDigits DO BEGIN
+          bigDig := 0;
+          FOR j := iBeg TO iBeg + lenOfFirst - 1 DO BEGIN
+            IF (s[j] >= '0') AND (s[j] <= '9') THEN
+              decDig := Ord(s[j]) - Ord('0')
+            ELSE BEGIN
+              WriteWarning(j);
+              decDig := 0;
+            END; (*ELSE*)
+            bigDig := bigDig * 10 + decDig;
+          END; (*FOR*)
+          Prepend(bi, bigDig);
+          iBeg := iBeg + lenOfFirst;
+          lenOfFirst := 3;
+        END; (*FOR*)
+(*$IFDEF SIGNED*)
+        Prepend(bi, sign);
+(*$ENDIF*)
+      END; (*IF*)
+    END; (*ELSE*)
+  END; (*ReadBigInt*)
+
+
+  (* WriteBigInt: writes BigIntPtr, version for base = 1000
+  -------------------------------------------------------*)
+  PROCEDURE WriteBigInt(bi: BigIntPtr);
+    VAR
+      revBi: BigIntPtr;
+      n: NodePtr;
+  BEGIN
+    IF base <> 1000 THEN BEGIN
+      WriteLn('Error in WriteBigInt: ',
+              'procedure currently works for base = 1000 only');
+      Halt;
+    END; (*IF*)
+    IF bi = NIL THEN
+      Write('0')
+    ELSE BEGIN
+(*$IFDEF SIGNED*)
+        IF Sign(bi) = -1 THEN
+          Write('-');
+        revBi := CopyOfBigInt(bi^.next);
+(*$ELSE*)
+        revBi := CopyOfBigInt(bi);
+(*$ENDIF*)
+      InvertBigInt(revBi);
+      n := revBi;
+      Write(n^.val); (*first big digit printed without leading zeros*)
+      n := n^.next;
+      WHILE n <> NIL DO BEGIN
+        IF n^.val >= 100 THEN
+          Write(n^.val)
+        ELSE IF n^.val >= 10 THEN
+          Write('0', n^.val)
+        ELSE (*n^.val < 10*)
+          Write('00', n^.val);
+        n := n^.next;
+      END; (*WHILE*)
+      DisposeBigInt(revBi); (*release the copy*)
+    END; (*IF*)
+  END; (*WriteBigInt*)
+
+FUNCTION ANZ_Nodes(a : BigIntPtr): INTEGER;
+VAR count : INTEGER;
+BEGIN
+  count := 1;
+  a := a^.next;
+  
+  WHILE a^.next <> NIL DO
+  BEGIN
+    a := a^.next;
+    count := count + 1;
+  END;
+  
+  ANZ_Nodes := count;
+END;
+
+FUNCTION Sum (a, b: BigIntPtr) : BigIntPtr;  (*compute sum = a + b*)
+VAR result, temp_a, temp_b : BigIntPtr;
+VAR sign_a, sign_b, overflow, temp, anz_a, anz_b, ishigher : Integer;
+
+BEGIN
+
+  IF a^.val = 0 THEN result := CopyOfBigInt(b);
+  IF b^.val = 0 THEN result := CopyOfBigInt(a)
+  ELSE
+  BEGIN 
+    result := NIL;
+    overflow := 0;
+    sign_a := Sign(a);
+    sign_b := Sign(b);
+    anz_a := ANZ_Nodes(a);
+    anz_b := ANZ_Nodes(b);
+
+    IF anz_a = anz_b THEN
+    BEGIN
+      temp_a := CopyOfBigInt(a);
+      temp_b := CopyOfBigInt(b);
+      InvertBigInt(temp_a);
+      InvertBigInt(temp_b);
+
+      WHILE (temp_a^.val = temp_b^.val) AND (temp_a^.next <> NIL) AND (temp_b^.next <> NIL) DO
+      BEGIN
+        temp_a := temp_a^.next;
+        temp_b := temp_b^.next;
+      END;
+      
+      IF (temp_a^.next = NIL) AND (temp_b^.next = NIL) THEN
+        ishigher := 0
+      ELSE IF temp_a^.val > temp_b^.val THEN ishigher := 1 
+      ELSE IF temp_a^.val < temp_b^.val THEN ishigher := 2
+
+    END
+    ELSE IF anz_a > anz_b THEN ishigher := 1
+    ELSE ishigher := 2;
+    
+    IF (sign_a = 1) AND (sign_b = 1) THEN Append(result,1)
+    ELSE IF (sign_a = -1) AND (sign_b = -1) THEN Append(result,-1)
+    ELSE IF (sign_a = -1) AND (sign_b = 1) THEN 
+      IF ishigher = 1 THEN Append(result,-1) ELSE Append(result,1)
+    ELSE
+      IF ishigher = 1 THEN Append(result,1) ELSE Append(result,-1);
+    
+    IF ishigher = 0 THEN
+    BEGIN
+      result^.val := 1;
+      Append(result,0);
+    END
+    ELSE
+    BEGIN
+      REPEAT
+
+        IF a^.next <> NIL THEN a := a^.next ELSE a^.val := 0;
+        IF b^.next <> NIL THEN b := b^.next ELSE b^.val := 0;
+
+        IF ((sign_a = 1) AND (sign_b = 1)) OR ((sign_a = -1) AND (sign_b = -1)) THEN temp := a^.val + b^.val + overflow
+        ELSE
+          IF ishigher = 1 THEN temp := a^.val - b^.val + overflow ELSE temp := b^.val - a^.val + overflow;    
+
+        IF temp >= 1000 THEN
+        BEGIN
+          overflow := 1;
+          temp := temp - 1000;
+        END
+        ELSE IF temp < 0 THEN
+          overflow := -1
+        ELSE
+          overflow := 0;  
+
+        Append(result,temp);
+
+      UNTIL (a^.next = NIL) AND (b^.next = NIL);
+    END;
+  END;
+  
+  Sum := result;
+END;
+
+FUNCTION Product (a, b: BigIntPtr) : BigIntPtr; (*compute product = a * b*)
+var result : BigIntPtr;
+
+BEGIN
+  result := NIL;
+
+  IF (a^.val = 0) OR (b^.val = 0) THEN 
+  BEGIN
+    Append(result, 1);
+    Append(result, 0);
+  END
+  ELSE
+  BEGIN 
+
+    IF (sign_a = 1) AND (sign_b = 1) THEN Append(result,1)
+    ELSE IF (sign_a = -1) AND (sign_b = -1) THEN Append(result,1)
+    ELSE
+      Append(result,-1);
+    
+    REPEAT
+      IF a^.next <> NIL THEN a := a^.next ELSE a^.val := 0;
+      IF b^.next <> NIL THEN b := b^.next ELSE b^.val := 0;
+
+      IF ((sign_a = 1) AND (sign_b = 1)) OR ((sign_a = -1) AND (sign_b = -1)) THEN temp := a^.val + b^.val + overflow
+      ELSE
+        IF ishigher = 1 THEN temp := a^.val - b^.val + overflow ELSE temp := b^.val - a^.val + overflow;    
+
+      IF temp >= 1000 THEN
+      BEGIN
+        overflow := 1;
+        temp := temp - 1000;
+      END
+      ELSE IF temp < 0 THEN
+        overflow := -1
+      ELSE
+        overflow := 0;  
+
+      Append(result,temp);
+
+    UNTIL (a^.next = NIL) AND (b^.next = NIL);
+  END;
+  
+  Sum := result;
+END;
+END;
+
+(*=== main program, for test purposes ===*)
+
+  VAR
+    bi : BigIntPtr;
+    bi2: BigIntPtr;
+    sumbi : BigIntPtr;
+
+BEGIN (*BigInts*)
+
+  WriteLn(chr(205),chr(205),chr(185),' BigInt ',chr(204),chr(205),chr(205));  
+  
+  (*tests for ReadBigInt and WriteBigInt only*)
+  Write('big int > ');
+  ReadBigInt(bi);
+  
+  Write('big int > ');
+  ReadBigInt(bi2);
+  
+  Write('big int 1 = ');
+  WriteBigInt(bi);
+  WriteLN;
+  
+  Write('big int 2 = ');
+  WriteBigInt(bi2);
+  WriteLn;
+  
+  Write('Sum: ');
+  sumbi := Sum(bi,bi2);
+  WriteBigInt(sumbi);
+  WriteLN;
+
+END. (*BigInts*)
+
